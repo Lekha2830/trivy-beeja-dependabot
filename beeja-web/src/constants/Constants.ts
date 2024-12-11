db8a1457623128e08a767da7c31de271683de60c
Lekha2830 <lekha@techatcore.com>
2024-12-11 13:47:30 +0530

    initial

---
+++ beeja-web/src/constants/Constants.ts
@@ -0,0 +1,38 @@
+// Create Profile Texts
+export const FirstNameRequired = 'First Name is required';
+export const LastNameRequired = 'Last Name is required';
+export const EmailRequired = 'Email is required';
+export const EmployeeIDRequired = 'Employee ID is required';
+export const ProfileCreationError = 'Error Happened, please try again later';
+export const ProfileCreationSuccessMessage = 'Created successfully!!';
+
+// Login Screen
+export const LoginText1 = "Let's empower your employees today.";
+export const LoginText2 =
+  'We help to complete all your conveyancing needs easily';
+export const SignInWithGoogleText = 'Sign in with Google';
+export const ERR_ACCOUNT_NOT_FOUND_TEXT =
+  'Login Unsuccessful - Please contact your HR Team';
+
+//Documnent Screen Texts
+export const DocumnentNameRequired = 'Document Type is Required';
+export const FileRequired = 'File is required';
+
+export const UploadFileError = 'Error Happened, please check the details again';
+
+export const MonitorLoanTermsAndConditions =
+  '';
+export const PersonalLoanTermsAndConditions =
+  '';
+
+export const minDateOfFromCalendar = new Date('01-20-2023');
+
+// ERROR CODES
+export const ERR_ACCOUNT_NOT_FOUND = 'NAF';
+
+export const PreviewReceiptError =
+  'Our system is facing difficulties, try again later';
+export const DownloadReceiptError = 'Error Happened, please try again later';
+export const ForbiddenError = 'No permissions';
+
+export const LOADING = 'Loading...';
